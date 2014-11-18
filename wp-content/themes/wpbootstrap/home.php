472f7bc2b22f2df72839cf610c9066b9225ea315
Emily Schleiner <cordial.emily@gmail.com>
2014-11-17 18:08:27 -0800

    comments fix and style posts on news page

--- wp-content/themes/wpbootstrap/home.php
+++ wp-content/themes/wpbootstrap/home.php
@@ -5,16 +5,19 @@
 
     	<h1>News</h1>
 
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	    
-		<p><em><?php the_time('l, F jS, Y'); ?></em></p>
-	    <hr>
-	    
-
-		<?php endwhile; else: ?>
-		  <p><?php _e('Sorry, there are no posts.'); ?></p>
-		<?php endif; ?>
+    	<div class="item">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		    
+			<p><em><?php the_time('l, F jS, Y'); ?></em></p>
+		</div>
+		    
+		    
+		<div class="item">
+			<?php endwhile; else: ?>
+			  <p><?php _e('Sorry, there are no posts.'); ?></p>
+			<?php endif; ?>
+		</div>
 	</div>
 
 	<div class="col-md-4, col-sm-4, col-xs-6">
--- wp-content/themes/wpbootstrap/page.php
+++ wp-content/themes/wpbootstrap/page.php
@@ -6,6 +6,7 @@
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	    <hr>
+	    
 	    <?php the_content(); ?>
 
 		<?php endwhile; else: ?>
