a39a94c8dc4ffe7433168b671671fcd4fc90cfa5
Emily Schleiner <cordial.emily@gmail.com>
2014-11-19 21:31:35 -0800

    news list fix

--- wp-content/themes/wpbootstrap/home.php
+++ wp-content/themes/wpbootstrap/home.php
@@ -1,26 +1,31 @@
 <?php get_header(); ?>
 
 <div class="row">
-    <div class="col-md-8, col-sm-8, col-xs-6">
+    <div class="col-xs-8">
 
     	<h1>News</h1>
 
-    	<div class="item">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	    	<div class="item">
+				
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			    
+				<p><em><?php the_time('l, F jS, Y'); ?></em></p>
+			</div>
+			
+		<?php endwhile; else: ?>
 		    
-			<p><em><?php the_time('l, F jS, Y'); ?></em></p>
-		</div>
-		    
-		    
-		<div class="item">
-			<?php endwhile; else: ?>
-			  <p><?php _e('Sorry, there are no posts.'); ?></p>
-			<?php endif; ?>
-		</div>
+			<div class="item">
+				
+				  <p><?php _e('Sorry, there are no posts.'); ?></p>
+				
+			</div>
+
+		<?php endif; ?>
+
 	</div>
 
-	<div class="col-md-4, col-sm-4, col-xs-6">
+	<div class="col-xs-4">
 		<?php get_sidebar(); ?>
 	</div>
 </div>
