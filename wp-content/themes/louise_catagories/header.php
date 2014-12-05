d458b4a397c009aadc646099c96bb745599a6534
Emily Schleiner <cordial.emily@gmail.com>
2014-12-04 17:48:57 -0800

    adding google fonts

--- wp-content/themes/louise_catagories/header.php
+++ wp-content/themes/louise_catagories/header.php
@@ -24,6 +24,10 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	
+	<!-- google fonts -->
+	<link href='http://fonts.googleapis.com/css?family=IM+Fell+English+SC|Lora|Cinzel' rel='stylesheet' type='text/css'>
+	
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
